30fe989f16ae0b33999d1c51d5b0aba441f3906d
Darrin Duncan <darrin@darrinduncan.com>
2021-03-20 11:11:02 -0400

    Initial Commit

---
+++ folio-ui/src/App.js
@@ -0,0 +1,18 @@
+import './App.scss';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Header from './components/Header'
+import Main from './components/Main'
+import Footer from './components/Footer'
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Main />
+      <Footer />
+
+    </>
+  );
+}
+
+export default App;
---
+++ folio-ui/src/components/Main.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Main() {
+    return (
+        <div>
+            <button className="btn btn-primary">Press Me</button>
+        </div>
+    )
+}
+
+export default Main
